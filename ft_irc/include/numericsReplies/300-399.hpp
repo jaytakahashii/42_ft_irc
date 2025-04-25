8fde20b7bec72c4d164cafc95cd094cb8e5034c9
kokosnakam <unknown@c3r1s3.42tokyo.jp>
2025-04-25 15:31:44 +0900

    feat(SRC): ✨  add error code
    error codes up to join are implemented

---
+++ ft_irc/include/numericsReplies/300-399.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+namespace irc {
+namespace numericReplies {
+
+// 332
+inline std::string RPL_TOPIC(const std::string& channel,
+                             const std::string& topic) {
+  return ":server 332 " + channel + " :" + topic + "\r\n";
+}
+
+}  // namespace numericReplies
+}  // namespace irc
