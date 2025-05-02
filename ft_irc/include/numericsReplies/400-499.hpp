1b8af533ba93abc562fd06d5750dda706ac1019a
Jay Takahashi <qsakura14555@gmail.com>
2025-05-02 17:48:28 +0900

    Revert "fix(SRC): 👾  edit numericsReplies (add target nick)"
    This reverts commit f8c620bc905696607baec34debcf33312e9ef55d.

--- ft_irc/include/numericsReplies/400-499.hpp
+++ ft_irc/include/numericsReplies/400-499.hpp
@@ -6,165 +6,135 @@ namespace irc {
 namespace numericReplies {
 
 // 402
-inline std::string ERR_NOSUCHSERVER(const std::string& serverName,
-                                    const std::string& nick) {
-  return ":irc.42tokyo.jp 402 " + nick + " " + serverName +
-         " :No such server\r\n";
+inline std::string ERR_NOSUCHSERVER(const std::string& serverName) {
+  return ":irc.42tokyo.jp 402 " + serverName + " :No such server\r\n";
 }
 
 // 403
-inline std::string ERR_NOSUCHCHANNEL(const std::string& channel,
-                                     const std::string& nick) {
-  return ":irc.42tokyo.jp 403 " + nick + " " + channel +
-         " :No such channel\r\n";
+inline std::string ERR_NOSUCHCHANNEL(const std::string& channel) {
+  return ":irc.42tokyo.jp 403 " + channel + " :No such channel\r\n";
 }
 
 // 405
-inline std::string ERR_TOOMANYCHANNELS(const std::string& channel,
-                                       const std::string& nick) {
-  return ":irc.42tokyo.jp 405 " + nick + " " + channel +
+inline std::string ERR_TOOMANYCHANNELS(const std::string& channel) {
+  return ":irc.42tokyo.jp 405 " + channel +
          " :You have joined too many channels\r\n";
 }
 
 // 407
 inline std::string ERR_TOOMANYTARGETS(const std::string& target,
                                       const std::string& errorCode,
-                                      const std::string& abortMessage,
-                                      const std::string& nick) {
-  return ":irc.42tokyo.jp 407 " + nick + " " + target + " :" + errorCode +
-         " recipients. " + abortMessage + "\r\n";
+                                      const std::string& abortMessage) {
+  return ":irc.42tokyo.jp 407 " + target + " :" + errorCode + " recipients. " +
+         abortMessage + "\r\n";
 }
 
 // 421
-inline std::string ERR_UNKNOWNCOMMAND(const std::string& command,
-                                      const std::string& nick) {
-  return ":irc.42tokyo.jp 421 " + nick + " " + command +
-         " :Unknown command\r\n";
+inline std::string ERR_UNKNOWNCOMMAND(const std::string& command) {
+  return ":irc.42tokyo.jp 421 " + command + " :Unknown command\r\n";
 }
 
 // 431
-inline std::string ERR_NONICKNAMEGIVEN(const std::string& nick) {
-  return ":irc.42tokyo.jp 431 " + nick + " :No nickname given\r\n";
+inline std::string ERR_NONICKNAMEGIVEN() {
+  return ":irc.42tokyo.jp 431 :No nickname given\r\n";
 }
 
 // 432
-inline std::string ERR_ERRONEUSNICKNAME(const std::string& nickname,
-                                        const std::string& nick) {
-  return ":irc.42tokyo.jp 432 " + nick + " " + nickname +
-         " :Erroneous nickname\r\n";
+inline std::string ERR_ERRONEUSNICKNAME(const std::string& nickname) {
+  return ":irc.42tokyo.jp 432 " + nickname + " :Erroneous nickname\r\n";
 }
 
 // 433
-inline std::string ERR_NICKNAMEINUSE(const std::string& nickname,
-                                     const std::string& nick) {
-  return ":irc.42tokyo.jp 433 " + nick + " " + nickname +
-         " :Nickname is already in use\r\n";
+inline std::string ERR_NICKNAMEINUSE(const std::string& nickname) {
+  return ":irc.42tokyo.jp 433 " + nickname + " :Nickname is already in use\r\n";
 }
 
 // 436
 inline std::string ERR_NICKCOLLISION(const std::string& nickname,
                                      const std::string& user,
-                                     const std::string& host,
-                                     const std::string& nick) {
-  return ":irc.42tokyo.jp 436 " + nick + " " + nickname +
-         " :Nickname collision KILL from " + user + "@" + host + "\r\n";
+                                     const std::string& host) {
+  return ":irc.42tokyo.jp 436 " + nickname + " :Nickname collision KILL from " +
+         user + "@" + host + "\r\n";
 }
 
 // 437
 inline std::string ERR_UNAVAILRESOURCE(const std::string& nickname,
-                                       const std::string& channel,
-                                       const std::string& nick) {
-  return ":irc.42tokyo.jp 437 " + nick + " " + nickname + "/" + channel +
+                                       const std::string& channel) {
+  return ":irc.42tokyo.jp 437 " + nickname + "/" + channel +
          " :Nick/channel is temporarily unavailable\r\n";
 }
 
 // 451
-inline std::string ERR_NOTREGISTERED(const std::string& nick) {
-  return ":irc.42tokyo.jp 451 " + nick + " :You have not registered\r\n";
+inline std::string ERR_NOTREGISTERED() {
+  return ":irc.42tokyo.jp 451 :You have not registered\r\n";
 }
 
 // 461
-inline std::string ERR_NEEDMOREPARAMS(const std::string& command,
-                                      const std::string& nick) {
-  return ":irc.42tokyo.jp 461 " + nick + " " + command +
-         " :Not enough parameters\r\n";
+inline std::string ERR_NEEDMOREPARAMS(const std::string& command) {
+  return ":irc.42tokyo.jp 461 " + command + " :Not enough parameters\r\n";
 }
 
 // 462
-inline std::string ERR_ALREADYREGISTRED(const std::string& nick) {
-  return ":irc.42tokyo.jp 462 " + nick +
-         " :Unauthorized command (already registered)\r\n";
+inline std::string ERR_ALREADYREGISTRED() {
+  return ":irc.42tokyo.jp 462 :Unauthorized command (already registered)\r\n";
 }
 
 // 464
-inline std::string ERR_PASSWDMISMATCH(const std::string& nick) {
-  return ":server 464 " + nick + " :Password incorrect\r\n";
+inline std::string ERR_PASSWDMISMATCH() {
+  return ":server 464 :Password incorrect\r\n";
 }
 
 // 471
-inline std::string ERR_CHANNELISFULL(const std::string& channel,
-                                     const std::string& nick) {
-  return ":irc.42tokyo.jp 471 " + nick + " " + channel +
-         " :Cannot join channel (+l)\r\n";
+inline std::string ERR_CHANNELISFULL(const std::string& channel) {
+  return ":irc.42tokyo.jp 471 " + channel + " :Cannot join channel (+l)\r\n";
 }
 
 // 472
-inline std::string ERR_UNKNOWNMODE(char mode, const std::string& channel,
-                                   const std::string& nick) {
-  return ":irc.42tokyo.jp 472 " + nick + " " + std::string(1, mode) +
+inline std::string ERR_UNKNOWNMODE(char mode, const std::string& channel) {
+  return ":irc.42tokyo.jp 472 " + std::string(1, mode) +
          " :is unknown mode char to me for " + channel + "\r\n";
 }
 
 // 473
-inline std::string ERR_INVITEONLYCHAN(const std::string& channel,
-                                      const std::string& nick) {
-  return ":irc.42tokyo.jp 473 " + nick + " " + channel +
-         " :Cannot join channel (+i)\r\n";
+inline std::string ERR_INVITEONLYCHAN(const std::string& channel) {
+  return ":irc.42tokyo.jp 473 " + channel + " :Cannot join channel (+i)\r\n";
 }
 
 // 474
-inline std::string ERR_BANNEDFROMCHAN(const std::string& channel,
-                                      const std::string& nick) {
-  return ":irc.42tokyo.jp 474 " + nick + " " + channel +
-         " :Cannot join channel (+b)\r\n";
+inline std::string ERR_BANNEDFROMCHAN(const std::string& channel) {
+  return ":irc.42tokyo.jp 474 " + channel + " :Cannot join channel (+b)\r\n";
 }
 
 // 475
-inline std::string ERR_BADCHANNELKEY(const std::string& channel,
-                                     const std::string& nick) {
-  return ":irc.42tokyo.jp 475 " + nick + " " + channel +
-         " :Cannot join channel (+k)\r\n";
+inline std::string ERR_BADCHANNELKEY(const std::string& channel) {
+  return ":irc.42tokyo.jp 475 " + channel + " :Cannot join channel (+k)\r\n";
 }
 
 // 476
-inline std::string ERR_BADCHANMASK(const std::string& channel,
-                                   const std::string& nick) {
-  return ":irc.42tokyo.jp 476 " + nick + " " + channel +
-         " :Bad Channel Mask\r\n";
+inline std::string ERR_BADCHANMASK(const std::string& channel) {
+  return ":irc.42tokyo.jp 476 " + channel + " :Bad Channel Mask\r\n";
 }
 
 // 481
-inline std::string ERR_NOPRIVILEGES(const std::string& nick) {
-  return ":irc.42tokyo.jp 481 " + nick +
-         " :Permission Denied - You're not an IRC "
+inline std::string ERR_NOPRIVILEGES() {
+  return ":irc.42tokyo.jp 481 :Permission Denied - You're not an IRC "
          "operator\r\n";
 }
 
 // 482
-inline std::string ERR_CHANOPRIVSNEEDED(const std::string& channel,
-                                        const std::string& nick) {
-  return ":irc.42tokyo.jp 482 " + nick + " :" + channel +
+inline std::string ERR_CHANOPRIVSNEEDED(const std::string& channel) {
+  return ":irc.42tokyo.jp 482 :" + channel +
          " :You're not channel operator\r\n";
 }
 
 // 484
-inline std::string ERR_RESTRICTED(const std::string& nick) {
-  return ":irc.42tokyo.jp 484 " + nick + " :Your connection is restricted!\r\n";
+inline std::string ERR_RESTRICTED() {
+  return ":irc.42tokyo.jp 484 :Your connection is restricted!\r\n";
 }
 
 // 491
-inline std::string ERR_NOOPERHOST(const std::string& nick) {
-  return ":irc.42tokyo.jp 491 " + nick + " :No O-lines for your host\r\n";
+inline std::string ERR_NOOPERHOST() {
+  return ":irc.42tokyo.jp 491 :No O-lines for your host\r\n";
 }
 
 }  // namespace numericReplies
