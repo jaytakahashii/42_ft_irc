289e99759c4e36a8ffbb858ea32e86fafa181130
Jay Takahashi <qsakura14555@gmail.com>
2025-05-03 13:30:35 +0900

    feat(SRC): ✨  add nick (001-099)

--- ft_irc/include/numericsReplies/001-099.hpp
+++ ft_irc/include/numericsReplies/001-099.hpp
@@ -5,20 +5,36 @@
 namespace irc {
 namespace numericReplies {
 
+// 001
 inline std::string RPL_WELCOME(const std::string& nick, const std::string& user,
                                const std::string& host) {
   return ":irc.42tokyo.jp 001 " + nick +
-         " Welcome to the Internet Relay Network " + nick + "!" + user + "@" +
+         " :Welcome to the Internet Relay Network " + nick + "!" + user + "@" +
          host + "\r\n";
 }
 
-inline std::string RPL_YOURHOST(const std::string& servername) {
-  return ":irc.42tokyo.jp Your host is " + servername +
+// 002
+inline std::string RPL_YOURHOST(const std::string& nick,
+                                const std::string& servername) {
+  return ":irc.42tokyo.jp 002 " + nick + " :Your host is " + servername +
          ", running version ft_irc\r\n";
 }
 
-inline std::string RPL_CREATED(const std::string& date) {
-  return ":irc.42tokyo.jp This server was created " + date + "\r\n";
+// 003
+inline std::string RPL_CREATED(const std::string& nick,
+                               const std::string& date) {
+  return ":irc.42tokyo.jp 003 " + nick + " :This server was created " + date +
+         "\r\n";
+}
+
+// 004
+inline std::string RPL_MYINFO(const std::string& nick,
+                              const std::string& servername,
+                              const std::string& version,
+                              const std::string& userModes,
+                              const std::string& channelModes) {
+  return ":irc.42tokyo.jp 004 " + nick + " " + servername + " " + version +
+         " " + userModes + " " + channelModes + "\r\n";
 }
 
 }  // namespace numericReplies
