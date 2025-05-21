6a298c1759c407284e7bc4df3b6ee2bdeb6a272f
jtakahas <jtakahas@student.42tokyo.jp>
2025-05-21 17:02:32 +0900

    feat(SRC): ✨  update file name

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,4 +1,4 @@
-import pytest
+import pytest # type: ignore
 import socket
 import yaml
 
@@ -6,19 +6,7 @@ def load_cases(file_path):
     with open(file_path, 'r') as f:
         return yaml.safe_load(f)
 
-@pytest.mark.parametrize("case", load_cases("cases/pass_nick_user.yml"), ids=lambda x: x["name"])
-def test_case(case):
-    sock = socket.create_connection(('localhost', 4242), timeout=3)
-    for step in case["steps"]:
-        sock.sendall((step["send"] + "\r\n").encode())
-
-    recv_data = sock.recv(4096).decode()
-    print("Received:", recv_data)  # デバッグ用
-    assert case["expect"] in recv_data
-    sock.close()
-
-
-@pytest.mark.parametrize("case", load_cases("cases/pass_nick_user.yml"), ids=lambda x: x["name"])
+@pytest.mark.parametrize("case", load_cases("cases/authentication.yml"), ids=lambda x: x["name"])
 def test_case(case):
     sock = socket.create_connection(('localhost', 4242), timeout=3)
     for step in case["steps"]:
